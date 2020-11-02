b01908df69ad7686e430765cef5bb48f9e71643d
Shoury Sharma <shanuu@pop-os.localdomain>
2020-11-02 23:53:21 +0530

    Added loadMusic from device

--- app/src/main/java/com/example/musixia/MainActivity.kt
+++ app/src/main/java/com/example/musixia/MainActivity.kt
@@ -1,12 +1,109 @@
 package com.example.musixia
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.ContentUris
+import android.content.pm.PackageManager
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import java.util.concurrent.TimeUnit
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        checkPermissions()
+    }
+
+    var CONTACT_CODE = 1234
+    fun checkPermissions(){
+        if(Build.VERSION.SDK_INT>=23){
+            if(ActivityCompat.checkSelfPermission(this,android.Manifest.permission.READ_EXTERNAL_STORAGE)!=
+                    PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),CONTACT_CODE)
+                return
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        when(requestCode){
+            CONTACT_CODE ->{
+                if(grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    Toast.makeText(this,"Permission granted",Toast.LENGTH_SHORT).show()
+                }else{
+                    Toast.makeText(this,"Permission rejected",Toast.LENGTH_SHORT).show()
+
+                }
+            }else -> super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        }
+    }
+
+    @RequiresApi(Build.VERSION_CODES.R)
+    fun loadMusic(){
+        data class Music(val uri: Uri,
+                         val name:String,
+                         val artist:String,
+                         val duration: Int,
+                         val size:Int)
+        var musicList = mutableListOf<Music>()
+
+        val projection = arrayOf(
+                MediaStore.Audio.Media._ID,
+                MediaStore.Audio.Media.DISPLAY_NAME,
+                MediaStore.Audio.Media.ARTIST,
+                MediaStore.Audio.Media.DURATION,
+                MediaStore.Audio.Media.SIZE
+                )
+
+        val selection = "${MediaStore.Audio.Media.DURATION} >= ?"
+        val selectionArgs = arrayOf(TimeUnit.MILLISECONDS.convert(1,TimeUnit.MINUTES)
+                .toString())
+        val sortOrder = "${MediaStore.Audio.Media.DISPLAY_NAME} ASC"
+        val query =  contentResolver.query(MediaStore.Audio.Media.EXTERNAL_CONTENT_URI,
+        projection,
+        selection,
+        selectionArgs,
+        sortOrder)
+
+        query?.use{
+            cursor ->
+            val idColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media._ID)
+            val nameColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DISPLAY_NAME)
+            val artistNameColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.ARTIST)
+            val durationColumn =
+                    cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.DURATION)
+            val sizeColumn = cursor.getColumnIndexOrThrow(MediaStore.Audio.Media.SIZE)
+
+
+            while(cursor.moveToNext()){
+                val id = cursor.getLong(idColumn)
+                val name = cursor.getString(nameColumn)
+                val artistName = cursor.getString(artistNameColumn)
+                val duration = cursor.getInt(durationColumn)
+                val size = cursor.getInt(sizeColumn)
+
+                val contentUri: Uri = ContentUris.withAppendedId(
+                        MediaStore.Video.Media.EXTERNAL_CONTENT_URI,
+                        id
+                )
+
+                // Save musics in list
+                musicList.add(Music(contentUri,name,artistName,duration,size))
+
+            }
+        }
+
     }
 
 }
