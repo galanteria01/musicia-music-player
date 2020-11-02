dc7b41ad9edfdb4c1dc08434803acb61c03d29d7
Shoury Sharma <shanuu@pop-os.localdomain>
2020-11-02 22:54:50 +0530

    Initial commit

---
+++ app/src/main/java/com/example/musixia/SplashActivity.kt
@@ -0,0 +1,22 @@
+package com.example.musixia
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+    val SPLASH_TIME = 2000L
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+            finish()
+        }, SPLASH_TIME)
+
+
+    }
+}
